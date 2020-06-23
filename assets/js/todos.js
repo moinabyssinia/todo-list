a7465b99d37be0842c79d1db8c27f0475e511f97
moinabyssinia <michaelg.tadesse@gmail.com>
2020-06-23 13:20:21 -0400

    Working on adding new element

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -9,17 +9,40 @@
 //select and manipulate the input 
 var inputTodo = document.querySelectorAll("input")[0];
 var todoRow = 1;
+
 //event listener for when "enter" key is pressed
 inputTodo.addEventListener("keydown", function(){
     if(event.keyCode === 13){
         console.log(this.value);
-        //use extra quotation marks and the plus signs
-        //to wrap a variable inside the css selector
-        document.querySelectorAll("h4:nth-of-type("+todoRow+")")[0].textContent = this.value;
-        document.querySelectorAll("h4:nth-of-type("+todoRow+")")[0].classList.toggle("embellishTodo");
-        //empty the input for next todo
-        document.querySelectorAll("input")[0].value = "";
-        todoRow +=1;
+
+        //create a new h4 element
+        var newH4 = document.createElement("h4");
+        //give it its content
+        var newContent = document.createTextNode("Hurray!");
+        //add the text node to the new h4
+        newH4.appendChild(newContent);
+        //insert the new h4 element
+        var lastP = document.getElementsByTagName("p");
+        document.body.insertBefore(newH4, lastP[0]);        
     }
 })
+
+
+
+
+
+
+// //event listener for when "enter" key is pressed
+// inputTodo.addEventListener("keydown", function(){
+//     if(event.keyCode === 13){
+//         console.log(this.value);
+//         //use extra quotation marks and the plus signs
+//         //to wrap a variable inside the css selector
+//         document.querySelectorAll("h4:nth-of-type("+todoRow+")")[0].textContent = this.value;
+//         document.querySelectorAll("h4:nth-of-type("+todoRow+")")[0].classList.toggle("embellishTodo");
+//         //empty the input for next todo
+//         document.querySelectorAll("input")[0].value = "";
+//         todoRow +=1;
+//     }
+// })
 
