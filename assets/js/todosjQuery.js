b677a9c004d5bd31a5a0d36a52ce4028595122a7
moinabyssinia <michaelg.tadesse@gmail.com>
2020-06-24 08:22:07 -0400

    Added new elements logic - jQuery

--- assets/js/todosjQuery.js
+++ assets/js/todosjQuery.js
@@ -1,11 +1,35 @@
-// // when the enter key is pressed
-// $("input").on("keypress", function(){
-//     if(event.keyCode === 13){
-//         $("h4").text() = $(input).value;
-//     }
-// })
+
+//add listener for when enter key is pressed
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //extract the typed text
+        /*         
+        console.log($("input[type = 'text']")[0].value);
+        console.log($("input[type='text']").text());
+        */
+        
+        console.log($(this).val());
+
+        //create a new element
+        $("ul").append("<li><span>X </span>"+$(this).val()+"</li>")
+        $("li").toggleClass("embellishTodo");
+        //clear out text input after hitting enter
+        $("input[type='text']").val("");
+    }
+    
+})
+
+//remove todos when the delete button is clicked
+$("body").on("click", "span", function(){
+    console.log("klicked");
+    
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
 
 // add line-through effect
-$("li").on("click", function(){
+$("body").on("click", "li",function(){
     $(this).toggleClass("lineThrough");
 })
