3438f9b108c08ca91ab5f0fc170011bc62f901b2
moinabyssinia <michaelg.tadesse@gmail.com>
2020-06-23 23:34:22 -0400

    First commit

--- assets/js/todosjQuery.js
+++ assets/js/todosjQuery.js
@@ -1,5 +1,11 @@
-$("input").on("keypress", function(){
-    if(event.keyCode === 13){
-        $("h4").text() = $(input).value;
-    }
+// // when the enter key is pressed
+// $("input").on("keypress", function(){
+//     if(event.keyCode === 13){
+//         $("h4").text() = $(input).value;
+//     }
+// })
+
+// add line-through effect
+$("li").on("click", function(){
+    $(this).toggleClass("lineThrough");
 })
