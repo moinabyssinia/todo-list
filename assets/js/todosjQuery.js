52246381b11daef7a8d9e0e77052b66042305957
moinabyssinia <michaelg.tadesse@gmail.com>
2020-06-26 06:57:04 -0400

    Added final style

--- assets/js/todosjQuery.js
+++ assets/js/todosjQuery.js
@@ -1,3 +1,4 @@
+// var inputCollapsed = false;
 
 //add listener for when enter key is pressed
 $("input[type='text']").keypress(function(event){
@@ -11,8 +12,7 @@ $("input[type='text']").keypress(function(event){
         console.log($(this).val());
 
         //create a new element
-        // $("ul").classList.add("noBullets");
-        $("ul").append("<li><span><i class='fas fa-trash-alt'></i> </span>"+$(this).val()+"</li>")
+        $("ul").append("<li><span><i class='fas fa-trash-alt'> </i></span>"+$(this).val()+"</li>")
         // $("li").toggleClass("embellishTodo");
         //clear out text input after hitting enter
         $("input[type='text']").val("");
@@ -24,6 +24,9 @@ $("input[type='text']").keypress(function(event){
 $("body").on("click", "span", function(){
     console.log("klicked");
     
+    //apply fading on the parent as it wraps the span
+    //and write a removing function inside fadeout
+    //inorder to wait until the fading finishes and then delete
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
@@ -34,3 +37,32 @@ $("body").on("click", "span", function(){
 $("body").on("click", "li",function(){
     $(this).toggleClass("lineThrough");
 })
+
+
+//fadeIn/Out input using the plus button
+$("h1 i").on("click", function(){
+    
+    // using fadeToggle
+    $("input").fadeToggle(500, function(){
+
+    });
+
+    // //using slideIn/Out
+    // if (inputCollapsed){
+    //     $("input").slideDown(500, function(){
+    //         inputCollapsed = false;
+    //     })
+    // } else{
+    //     $("input").slideUp(500, function(){
+    //         inputCollapsed = true;
+    //     })
+    // }
+
+})
+
+
+// //transition icon when hovered
+// $("body").on("mouseenter", "span", function(){
+//     console.log("hovered");
+//     // $(this.i).toggleClass("hovered")
+// })
